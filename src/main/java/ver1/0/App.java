05324b11b1c2b2af464466c2b73ffee6d7d5ccba
Dimitry Panasuk <daitin13@gmail.com>
2017-08-21 15:12:13 +0300

    'test'

---
+++ src/main/java/ver1/0/App.java
@@ -0,0 +1,15 @@
+package ver1.0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    String str = new String("jdfjdfsjhf");
+
+}
